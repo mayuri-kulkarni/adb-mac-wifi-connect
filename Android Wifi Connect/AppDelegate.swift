29de7fc7459d39b49a186087b66c4f7bce3502e8
mayuri <mayurimk47@gmail.com>
2020-04-24 15:05:46 +0530

    Initial Commit

---
+++ Android Wifi Connect/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Android Wifi Connect
+//
+//  Created by Chetan Gangurde on 24/04/20.
+//  Copyright © 2020 Mayuri Kulkarni. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Android Wifi Connect/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Android Wifi Connect
+//
+//  Created by Chetan Gangurde on 24/04/20.
+//  Copyright © 2020 Mayuri Kulkarni. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
